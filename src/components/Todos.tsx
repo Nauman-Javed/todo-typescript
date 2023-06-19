2884593e4adc97e81e0dd08b1ff5910c87d9e235
Nauman-Javed <naumanj841@gmail.com>
2023-06-19 14:58:00 +0500

    setup react and typescript project

---
+++ src/components/Todos.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <div>
+      <ul>
+        <li>Todos</li>
+        <li>List</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
